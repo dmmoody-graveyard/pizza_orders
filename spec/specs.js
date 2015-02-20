77a2ea4fd3805a744ec50f3e793e980000c29471
Duane M. Moody <dmmoody@gmail.com>
2015-02-20 10:19:59 -0800

    refactor slices specs to determine topping with size

--- spec/specs.js
+++ spec/specs.js
@@ -6,20 +6,23 @@ describe('Pizza', function() {
       expect(newPizza.topping).to.equal('Cheese');
     });
   });
-  
+
   describe('slices()', function() {
     it('takes 12 and returns 8', function() {
       var newPizza = Object.create(Pizza);
+      newPizza.topping = 'Cheese';
       expect(newPizza.slices(12)).to.equal(8);
     });
 
     it('takes 10 and returns 6', function() {
       var newPizza = Object.create(Pizza);
+      newPizza.topping = 'Cheese';
       expect(newPizza.slices(10)).to.equal(6);
     });
 
     it('takes 14 and returns 10', function() {
       var newPizza = Object.create(Pizza);
+      newPizza.topping = 'Cheese';
       expect(newPizza.slices(14)).to.equal(10);
     });
   });
