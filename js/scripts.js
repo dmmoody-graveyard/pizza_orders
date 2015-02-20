cfc6869cc7500d75390f25ab7566cf9f726df4c7
Duane M. Moody <dmmoody@gmail.com>
2015-02-20 10:22:49 -0800

    add logic to evaluate "Cheese" topping

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,13 @@
 var Pizza = {
   slices: function(size) {
-    if (size === 12) {
-      return 8;
-    } else if (size === 10) {
-      return 6;
-    } else {
-      return 10;
+    if (this.topping === "Cheese") {
+      if (size === 12) {
+        return 8;
+      } else if (size === 10) {
+        return 6;
+      } else {
+        return 10;
+      }
     }
   }
 };
