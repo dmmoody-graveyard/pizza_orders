cb1a1ae32b620b8a8689bdd0ac05edc79d461ece
Duane M. Moody <dmmoody@gmail.com>
2015-02-20 09:56:28 -0800

    add spec for 10 inch pizza

--- spec/specs.js
+++ spec/specs.js
@@ -1,8 +1,13 @@
 describe('Pizza', function() {
-    describe('slices()', function() {
-        it('takes 12 and returns 8', function() {
-            var newPizza = Object.create(Pizza);
-            expect(newPizza.slices(12)).to.equal(8);
-        });
+  describe('slices()', function() {
+    it('takes 12 and returns 8', function() {
+      var newPizza = Object.create(Pizza);
+      expect(newPizza.slices(12)).to.equal(8);
     });
+
+    it('takes 10 and returns 6', function() {
+        var newPizza = Object.create(Pizza);
+        expect(newPizza.slices(10)).to.equal(6);
+    });
+  });
 });
