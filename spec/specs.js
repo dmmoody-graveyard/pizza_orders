58105bd33462247f546cae5e66504ed2cb5d6b66
Duane M. Moody <dmmoody@gmail.com>
2015-02-20 10:28:33 -0800

    add spec for pepperoni pizza slices

--- spec/specs.js
+++ spec/specs.js
@@ -14,22 +14,28 @@ describe('Pizza', function() {
   });
 
   describe('slices()', function() {
-    it('takes 12 and returns 8', function() {
+    it('takes 12 for a Cheese pizza and returns 8', function() {
       var newPizza = Object.create(Pizza);
       newPizza.topping = 'Cheese';
       expect(newPizza.slices(12)).to.equal(8);
     });
 
-    it('takes 10 and returns 6', function() {
+    it('takes 10 for a Cheese pizza and returns 6', function() {
       var newPizza = Object.create(Pizza);
       newPizza.topping = 'Cheese';
       expect(newPizza.slices(10)).to.equal(6);
     });
 
-    it('takes 14 and returns 10', function() {
+    it('takes 14 for a Cheese pizza and returns 10', function() {
       var newPizza = Object.create(Pizza);
       newPizza.topping = 'Cheese';
       expect(newPizza.slices(14)).to.equal(10);
+    });
+
+    it('takes 12 for a Pepperoni pizza and returns 6', function() {
+      var newPizza = Object.create(Pizza);
+      newPizza.topping = "Pepperoni";
+      expect(newPizza.slices(12)).to.equal(6);
     });
   });
 });
